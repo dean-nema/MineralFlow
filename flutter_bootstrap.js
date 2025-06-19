70d7eaa409a1087f837543c328349dd7d4bc9c01
dean-nema <139811805+dean-nema@users.noreply.github.com>
2025-06-19 01:38:51 +0000

    Deploying to gh-pages from @ dean-nema/MineralFlow@fc99cc648c497401aa7d148008eca0dcb174ed2c 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3450910609"
+    serviceWorkerVersion: "37848474"
   }
 });
